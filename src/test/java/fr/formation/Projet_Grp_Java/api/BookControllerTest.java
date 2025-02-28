f87a662b00a7b23a047facdacc8e725a773db22d
Florent <florent.guyard@live.fr>
2025-02-28 14:03:50 +0100

    amélioration setUp des tests - instance objet au début

--- src/test/java/fr/formation/Projet_Grp_Java/api/BookControllerTest.java
+++ src/test/java/fr/formation/Projet_Grp_Java/api/BookControllerTest.java
@@ -15,15 +15,9 @@ import fr.formation.Projet_Grp_Java.model.BookFormat;
 import fr.formation.Projet_Grp_Java.repo.BookRepository;
 import fr.formation.Projet_Grp_Java.service.BookService;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BookControllerTest {
 
@@ -38,138 +32,50 @@ public class BookControllerTest {
     @InjectMocks
     private BookController bookController;
 
+    private Book bookTest1;
+    private Book bookTest2;
+    private Book bookTest3;
+    private List<Book> books;
+
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this); // Initialise les mocks
+        MockitoAnnotations.openMocks(this);
+
+        bookTest1 = new Book("1", "2070405370", "Le Comte de Monte-Cristo", "Alexandre Dumas", "Le Livre de Poche",
+                POCHE, true);
+        bookTest2 = new Book("2", "2701192919", "Pauline", "Alexandre Dumas", "Le Livre de Poche", POCHE, true);
+        bookTest3 = new Book("3", "2701192917", "Un mauvais livre", "Un mauvais auteur", "Un mauvais éditeur", POCHE,
+                false);
+
+        books = Arrays.asList(bookTest1, bookTest2);
     }
 
     @Test
     void testGetAllBooks() {
-        Book bookTest1 = new Book();
-        bookTest1.setId("1");
-        bookTest1.setIsbn("2070405370");
-        bookTest1.setTitle("Le Comte de Monte-Cristo");
-        bookTest1.setAuthor("Alexandre Dumas");
-        bookTest1.setPublisher("Le Livre de Poche");
-        bookTest1.setFormat(POCHE);
-        bookTest1.setAvailable(true);
-
-        Book bookTest2 = new Book();
-        bookTest2.setId("2");
-        bookTest2.setIsbn("2701192919");
-        bookTest2.setTitle("Pauline");
-        bookTest2.setAuthor("Alexandre Dumas");
-        bookTest2.setPublisher("Le Livre de Poche");
-        bookTest2.setFormat(POCHE);
-        bookTest2.setAvailable(true);
-
-        List<Book> books = Arrays.asList(bookTest1, bookTest2);
-
-        // WHEN :
-
         when(bookRepository.findAll()).thenReturn(books);
-
         List<Book> result = bookController.getAllBooks();
-
         assertEquals(2, result.size());
     }
 
     @Test
     void testGetBookById() {
-        Book bookTest1 = new Book();
-        bookTest1.setId("1");
-        bookTest1.setIsbn("2070405370");
-        bookTest1.setTitle("Le Comte de Monte-Cristo");
-        bookTest1.setAuthor("Alexandre Dumas");
-        bookTest1.setPublisher("Le Livre de Poche");
-        bookTest1.setFormat(POCHE);
-        bookTest1.setAvailable(true);
-
-        Book bookTest2 = new Book();
-        bookTest2.setId("2");
-        bookTest2.setIsbn("2701192919");
-        bookTest2.setTitle("Pauline");
-        bookTest2.setAuthor("Alexandre Dumas");
-        bookTest2.setPublisher("Le Livre de Poche");
-        bookTest2.setFormat(POCHE);
-        bookTest2.setAvailable(true);
-
-        // WHEN :
-
         when(bookRepository.findById("1")).thenReturn(Optional.of(bookTest1));
-
         assertEquals(bookTest1, bookController.getBookById("1").getBody());
     }
 
     @Test
     void testGetBookByIsbn() {
-        Book bookTest1 = new Book();
-        bookTest1.setId("1");
-        bookTest1.setIsbn("2070405370");
-        bookTest1.setTitle("Le Comte de Monte-Cristo");
-        bookTest1.setAuthor("Alexandre Dumas");
-        bookTest1.setPublisher("Le Livre de Poche");
-        bookTest1.setFormat(POCHE);
-        bookTest1.setAvailable(true);
-
-        Book bookTest2 = new Book();
-        bookTest2.setId("2");
-        bookTest2.setIsbn("2701192919");
-        bookTest2.setTitle("Pauline");
-        bookTest2.setAuthor("Alexandre Dumas");
-        bookTest2.setPublisher("Le Livre de Poche");
-        bookTest2.setFormat(POCHE);
-        bookTest2.setAvailable(true);
-
-        List<Book> books = Arrays.asList(bookTest1, bookTest2);
-
-        // WHEN :
-
         when(bookService.getBooksByIsbn("2070405370")).thenReturn(books);
-
         assertEquals(books, bookController.getBooksByIsbn("2070405370"));
     }
 
     @Test
     void getBooksByTitle() {
-        Book bookTest1 = new Book();
-        bookTest1.setId("1");
-        bookTest1.setIsbn("2070405370");
-        bookTest1.setTitle("Le Comte de Monte-Cristo");
-        bookTest1.setAuthor("Alexandre Dumas");
-        bookTest1.setPublisher("Le Livre de Poche");
-        bookTest1.setFormat(POCHE);
-        bookTest1.setAvailable(true);
-
-        Book bookTest2 = new Book();
-        bookTest2.setId("2");
-        bookTest2.setIsbn("2701192919");
-        bookTest2.setTitle("Pauline");
-        bookTest2.setAuthor("Alexandre Dumas");
-        bookTest2.setPublisher("Le Livre de Poche");
-        bookTest2.setFormat(POCHE);
-        bookTest2.setAvailable(true);
-
-        Book bookTest3 = new Book();
-        bookTest3.setId("3");
-        bookTest3.setIsbn("2701192917");
-        bookTest3.setTitle("Un mauvais livre");
-        bookTest3.setAuthor("Un mauvais auteur");
-        bookTest3.setPublisher("Un mauvais éditeur");
-        bookTest3.setFormat(POCHE);
-        bookTest3.setAvailable(false);
-
-        List<Book> expectedBooks = Arrays.asList(bookTest1);
-
-        // WHEN :
-
-        // On recherche un livre par son auteur et on ne tient pas compte de la casse
-        // (majuscule/minuscule)
+        List<Book> expectedBooks = List.of(bookTest1);
         when(bookService.getBooksByTitle("Le Comte de Monte-Cristo")).thenReturn(expectedBooks);
 
         List<Book> result = bookController.getBooksByTitle("Le Comte de Monte-Cristo");
 
-        // THEN :
         assertEquals(expectedBooks, result);
         assertEquals(1, result.size());
         assertTrue(result.contains(bookTest1));
@@ -178,165 +84,52 @@ public class BookControllerTest {
 
     @Test
     void getBooksByAuthor() {
-
-        Book bookTest1 = new Book();
-        bookTest1.setId("1");
-        bookTest1.setIsbn("2070405370");
-        bookTest1.setTitle("Le Comte de Monte-Cristo");
-        bookTest1.setAuthor("Alexandre Dumas");
-        bookTest1.setPublisher("Le Livre de Poche");
-        bookTest1.setFormat(POCHE);
-        bookTest1.setAvailable(true);
-
-        Book bookTest2 = new Book();
-        bookTest2.setId("2");
-        bookTest2.setIsbn("2701192919");
-        bookTest2.setTitle("Pauline");
-        bookTest2.setAuthor("Alexandre Dumas");
-        bookTest2.setPublisher("Le Livre de Poche");
-        bookTest2.setFormat(POCHE);
-        bookTest2.setAvailable(true);
-
-        Book bookTest3 = new Book();
-        bookTest3.setId("3");
-        bookTest3.setIsbn("2701192917");
-        bookTest3.setTitle("Un mauvais livre");
-        bookTest3.setAuthor("Un mauvais auteur");
-        bookTest3.setPublisher("Un mauvais éditeur");
-        bookTest3.setFormat(POCHE);
-        bookTest3.setAvailable(false);
-
-        List<Book> expectedBooks = Arrays.asList(bookTest1, bookTest2);
-
-        // WHEN :
+        List<Book> expectedBooks = List.of(bookTest1, bookTest2);
         when(bookService.getBooksByAuthor("Alexandre Dumas")).thenReturn(expectedBooks);
 
-        // List<Book> result = bookController.getBooksByAuthor("Alexandre Dumas");
         List<Book> result = bookService.getBooksByAuthor("Alexandre Dumas");
 
-        // THEN :
-        // assertEquals(expectedBooks, result);
         assertTrue(result.contains(bookTest1));
         assertTrue(result.contains(bookTest2));
         assertFalse(result.contains(bookTest3));
     }
 
     @Test
-    void getBooksByIsbn() {
-        Book bookTest1 = new Book();
-        bookTest1.setId("1");
-        bookTest1.setIsbn("2070405370");
-        bookTest1.setTitle("Le Comte de Monte-Cristo");
-        bookTest1.setAuthor("Alexandre Dumas");
-        bookTest1.setPublisher("Le Livre de Poche");
-        bookTest1.setFormat(POCHE);
-        bookTest1.setAvailable(true);
-
-        Book bookTest2 = new Book();
-        bookTest2.setId("2");
-        bookTest2.setIsbn("2701192919");
-        bookTest2.setTitle("Pauline");
-        bookTest2.setAuthor("Alexandre Dumas");
-        bookTest2.setPublisher("Le Livre de Poche");
-        bookTest2.setFormat(POCHE);
-        bookTest2.setAvailable(true);
-
-        Book bookTest3 = new Book();
-        bookTest3.setId("3");
-        bookTest3.setIsbn("2701192917");
-        bookTest3.setTitle("Un mauvais livre");
-        bookTest3.setAuthor("Un mauvais auteur");
-        bookTest3.setPublisher("Un mauvais éditeur");
-        bookTest3.setFormat(POCHE);
-        bookTest3.setAvailable(false);
-
-        List<Book> expectedBookGoodIsbn = Arrays.asList(bookTest1);
-
-        when(bookService.getBooksByIsbn("2070405370")).thenReturn(expectedBookGoodIsbn);
-
-        List<Book> result = bookController.getBooksByIsbn("2070405370");
-
-        // THEN :
-        assertEquals(expectedBookGoodIsbn, result);
-        assertEquals(1, result.size());
-        assertTrue(result.contains(bookTest1));
-        assertFalse(result.contains(bookTest2));
-
-    }
-
-    @Test
     void createBookWithCompleteData() {
-        Book bookTest1 = new Book();
-        bookTest1.setId("1");
-        bookTest1.setIsbn("2070405370");
-        bookTest1.setTitle("Le Comte de Monte-Cristo");
-        bookTest1.setAuthor("Alexandre Dumas");
-        bookTest1.setPublisher("Le Livre de Poche");
-        bookTest1.setFormat(POCHE);
-        bookTest1.setAvailable(true);
-
-        // WHEN :
-        // J appelle deux fois la méthode createBook de mon service donc l'asseertion
-        // compte deux appels
         when(bookService.createBook(bookTest1)).thenReturn(bookTest1);
 
         Book result = bookService.createBook(bookTest1);
 
-        // THEN :
         assertEquals(bookTest1, bookController.createBook(bookTest1));
         assertNotNull(result);
         assertEquals(bookTest1, result);
         verify(bookService, times(2)).createBook(any(Book.class));
-
     }
 
     @Test
     void createBookWithIncompleteData() {
-
-        Book bookTest2 = new Book();
-        bookTest2.setId("2");
-        bookTest2.setIsbn("INVALID_ISBN"); // ISBN invalide ici
-        bookTest2.setTitle("Le Comte de Monte-Cristo");
-        bookTest2.setAuthor("Alexandre Dumas");
-        bookTest2.setPublisher("Le Livre de Poche");
-        bookTest2.setFormat(POCHE);
-        bookTest2.setAvailable(true);
-
+        bookTest2.setIsbn("INVALID_ISBN");
         when(bookService.createBook(bookTest2)).thenReturn(null);
 
-        // WHEN
         Book result = bookController.createBook(bookTest2);
 
-        // THEN
         assertNull(result);
         verify(bookService, times(1)).createBook(any(Book.class));
     }
 
     @Test
     void createBookWithNullFields() {
-
-        Book bookTest3 = new Book();
-        bookTest3.setId("3");
-        bookTest3.setIsbn("2070405370");
         bookTest3.setTitle(null);
         bookTest3.setAuthor(null);
         bookTest3.setPublisher(null);
         bookTest3.setFormat(null);
 
-        Book expectedBook = new Book();
-        expectedBook.setId("3");
-        expectedBook.setIsbn("2070405370");
-        expectedBook.setTitle("test");
-        expectedBook.setAuthor("test");
-        expectedBook.setPublisher("test");
-        expectedBook.setFormat(BookFormat.BROCHE); // C'est la valeur que je mets par défaut
+        Book expectedBook = new Book("3", "2070405370", "test", "test", "test", BookFormat.BROCHE, true);
 
         when(bookService.createBook(any(Book.class))).thenReturn(expectedBook);
 
-        // WHEN
         Book result = bookController.createBook(bookTest3);
 
-        // THEN
         assertNotNull(result);
         assertEquals("test", result.getTitle());
         assertEquals("test", result.getAuthor());
